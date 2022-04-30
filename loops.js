e9bb4b11b658479f6176029a43ab0db589ffe53a
enzojnardone <101941591+enzojnardone@users.noreply.github.com>
2022-04-30 19:02:47 +0200

    iteracion 2

--- loops.js
+++ loops.js
@@ -30,3 +30,18 @@ for (const places of placesToTravel) {
     console.log(places)   
 }
 
+// **Iteración #4: Probando For...in**
+
+// Usa un **for...in** para imprimir por consola los datos del alienígena.. Puedes usar este objeto:
+
+const alien = {
+    name: 'Wormuck',
+    race: 'Cucusumusu',
+    planet: 'Eden',
+    weight: '259kg'
+}
+
+for (const key in alien) {
+    console.log(alien[key])      
+}
+
